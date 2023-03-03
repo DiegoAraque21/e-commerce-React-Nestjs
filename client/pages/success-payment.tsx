bda18921f918cc3d7206e17e0f84cc86aea8d913
DiegoAraque21 <diego.araque.fernandez@gmail.com>
2023-03-03 11:15:26 -0600

    not access successful if no client_secret

--- client/pages/success-payment.tsx
+++ client/pages/success-payment.tsx
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import Page from '@/components/Page';
+import CircularProgress from '@mui/material/CircularProgress';
 
 function success() {
-  return (
+  const router = useRouter();
+  const [notLoading, setIsNotLoading] = useState(true);
+
+  useEffect(() => {
+    // check payment-intent url and payment_intent_client_secret
+    const url = window.location.href;
+    const paymentIntent = url.split('payment_intent=')[1];
+    const clientSecret = url.split('payment_intent_client_secret=')[1];
+    if (!paymentIntent || !clientSecret) {
+      router.push('/');
+    } else {
+      setIsNotLoading(false);
+    }
+  }, []);
+
+  console.log('notLoading: ', notLoading);
+  if (notLoading) {
     <Page>
-      <div className="ml-10 font-bold text-2xl">
-        Payment completed. Thanks for your order!
+      <div className="w-full flex">
+        <CircularProgress className="mx-auto mt-[100px]" />
       </div>
-    </Page>
-  );
+    </Page>;
+  } else {
+    return (
+      <Page>
+        <div className="ml-10 font-bold text-2xl">
+          Payment completed. Thanks for your order!
+        </div>
+      </Page>
+    );
+  }
 }
 
 export default success;
